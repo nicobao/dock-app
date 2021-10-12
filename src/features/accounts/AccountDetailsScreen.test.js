1d52fa4489cbc63df90fe389ac8ecee9eadb0ba1
Maycon Mellos <maycon.mellos@gmail.com>
2021-10-12 10:30:08 -0300

    add unit tests

--- src/features/accounts/AccountDetailsScreen.test.js
+++ src/features/accounts/AccountDetailsScreen.test.js
@@ -1,7 +1,7 @@
 import {shallow} from 'enzyme';
 import React from 'react';
 import configureMockStore from 'redux-mock-store';
-import {AccountDetailsScreen} from './AccountDetailsScreen';
+import {AccountDetailsScreen, filterTransactionHistory} from './AccountDetailsScreen';
 
 const mockStore = configureMockStore();
 
@@ -25,5 +25,56 @@ describe('AccountDetailsScreen', () => {
       },
     );
     expect(wrapper.dive()).toMatchSnapshot();
+  });
+
+  describe('expect to filter transaction history items', () => {
+    const address1 = '3C7Hq5jQGxeYzL7LnVASn48tEfr6D7yKtNYSuXcgioQoWWsB';
+    const address2 = '4C7Hq5jQGxeYzL7LnVASn48tEfr6D7yKtNYSuXcgioQoWWsB';
+    const txSent = {
+      amount: "1",
+      fromAddress: address1,
+      recipientAddress: address2,
+      status: 'complete'
+    };
+    const txSentFailed = {
+      amount: "1",
+      fromAddress: address1,
+      recipientAddress: address2,
+      status: 'failed'
+    };
+    const txReceived = {
+      amount: "1",
+      fromAddress: address2,
+      recipientAddress: address1,
+      status: 'complete'
+    };
+    const txReceivedFailed = {
+      amount: "1",
+      fromAddress: address2,
+      recipientAddress: address1,
+      status: 'failed'
+    };
+    const txOtherAddress = {
+      amount: "1",
+      fromAddress: 'someOtherAddress',
+      recipientAddress: 'someOtherAddress',
+      status: 'complete'
+    };
+
+    it('expect to hide received transactions with failed status', () => {
+      const transactions = filterTransactionHistory([txReceived, txReceivedFailed], address1);
+      expect(transactions.length).toBe(1);
+    });
+
+    it('expect to add sent flag', () => {
+      const transactions = filterTransactionHistory([txReceived, txSent], address1);
+      expect(transactions[0].sent).toBe(false);
+      expect(transactions[1].sent).toBe(true);
+    });
+
+    it('expect to filter only transactions for the given address', () => {
+      const transactions = filterTransactionHistory([txOtherAddress, txSent], address1);
+      expect(transactions.length).toBe(1);
+    });
   });
 });
