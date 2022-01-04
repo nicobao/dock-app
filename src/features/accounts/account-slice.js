f01974b9ba6d4f5cba3671dacbf8d52d8e461222
Maycon Mello <maycon.mellos@gmail.com>
2022-01-04 18:53:25 -0300

    fixing issue with loading accounts

--- src/features/accounts/account-slice.js
+++ src/features/accounts/account-slice.js
@@ -243,8 +243,6 @@ export const accountOperations = {
 
     await WalletRpc.sync();
 
-    await WalletRpc.sync();
-
     let accounts = await WalletRpc.query({
       equals: {
         'content.type': 'Account',
@@ -252,7 +250,7 @@ export const accountOperations = {
     });
 
     if (!Array.isArray(accounts)) {
-      throw new Error('Invalid accounts data');
+      return;
     }
 
     accounts = accounts.map(account => {
