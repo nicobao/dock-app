d72e52ef733a53a1b07e8c56f32e78a582d9289c
Samuel Hellawell <sshellawell@gmail.com>
2022-01-11 07:59:43 +0000

    fix send to form state isnt reset after success

--- src/features/tokens/SendTokenScreen.js
+++ src/features/tokens/SendTokenScreen.js
@@ -145,21 +145,23 @@ const Steps = {
   enterAmount: 2,
 };
 
+const defaultFormState = {
+  recipientAddress: '',
+  amount: 0,
+  tokenSymbol: 'DOCK',
+  fee: 0,
+  validating: false,
+  _errors: {},
+  _hasError: false,
+};
+
 export function SendTokenContainer({route}) {
   const dispatch = useDispatch();
   const {address} = route.params || {};
   const accountDetails = useSelector(accountSelectors.getAccountById(address));
   const [showConfirmation, setShowConfirmation] = useState();
   const [step, setStep] = useState(Steps.sendTo);
-  const [form, setForm] = useState({
-    recipientAddress: '',
-    amount: 0,
-    tokenSymbol: 'DOCK',
-    fee: 0,
-    validating: false,
-    _errors: {},
-    _hasError: false,
-  });
+  const [form, setForm] = useState(defaultFormState);
 
   const handleCopyAddress = () => {
     Clipboard.setString(address);
@@ -261,6 +263,8 @@ export function SendTokenContainer({route}) {
               navigate(Routes.ACCOUNT_DETAILS, {
                 id: accountDetails.id,
               });
+              setForm(defaultFormState);
+              setStep(Steps.sendTo);
             });
           }}
           amountMessage={form.amountMessage}
