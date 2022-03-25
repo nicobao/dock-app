e7db1a3b41b0491c7b4d5766e3236f3ef89148ab
Ifeanyi Osinakayah <ifeanyiosinakayah15@gmail.com>
2022-03-25 17:10:12 +0100

    Include unit test for en translation

--- src/features/wallet/wallet-slice.test.js
+++ src/features/wallet/wallet-slice.test.js
@@ -1,7 +1,7 @@
 import {translate} from 'src/locales';
 
 describe('Wallet Slice', () => {
-  it('expect to remove account from store', () => {
+  it('expect error message to be accurate', () => {
     expect('Invalid backup file or incorrect password').toBe(
       translate('import_wallet.import_error', {
         locale: 'en',
