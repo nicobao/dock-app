f0b82e0f5ea87d66273bfd15e06ad0bb85db6300
Maycon Mellos <maycon.mellos@gmail.com>
2022-03-29 11:03:56 -0300

    Credentials storage

--- src/features/credentials/credentials.js
+++ src/features/credentials/credentials.js
@@ -2,11 +2,7 @@ import {useEffect, useState} from 'react';
 import testCredential from './test-credential.json';
 import {Credentials} from '@docknetwork/wallet-sdk-credentials/lib';
 export function useCredentials() {
-  const [items, setItems] = useState([
-    testCredential,
-    {...testCredential},
-    {...testCredential},
-  ]);
+  const [items, setItems] = useState([]);
 
   const syncCredentials = async () => {
     const credentials = await Credentials.getInstance().query();
