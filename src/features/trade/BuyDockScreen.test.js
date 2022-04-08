d47c39e23ab6023464d0c358315c4b76ec9f594e
Ifeanyi Osinakayah <ifeanyiosinakayah15@gmail.com>
2022-04-08 19:38:34 +0100

    Snapshots

--- src/features/trade/BuyDockScreen.test.js
+++ src/features/trade/BuyDockScreen.test.js
@@ -3,6 +3,8 @@ import React from 'react';
 import configureMockStore from 'redux-mock-store';
 import {BuyDockScreenScreen} from './BuyDockScreen';
 import thunk from 'redux-thunk';
+import {renderAppProviders} from '../../core/test-utils';
+import TransakProvider from './components/TransakProvider';
 
 const mockStore = configureMockStore([thunk]);
 
@@ -15,11 +17,23 @@ describe('BuyDockScreenScreen', () => {
     const wrapper = shallow(
       <BuyDockScreenScreen
         walletAddress={'3C7Hq5jQGxeYzL7LnVASn48tEfr6D7yKtNYSuXcgioQoWWsB'}
-        partnerOrderId={'fd1b4a14-f4be-4218-9e76-4aec9c00db69'}
+        orderId={'fd1b4a14-f4be-4218-9e76-4aec9c00db69'}
       />,
       {
         context: {store: mockStore(initialState)},
       },
+    );
+    expect(wrapper.dive()).toMatchSnapshot();
+  });
+
+  it('should render TransakProvider correctly', () => {
+    const wrapper = shallow(
+      renderAppProviders(
+        <TransakProvider
+          walletAddress={'3C7Hq5jQGxeYzL7LnVASn48tEfr6D7yKtNYSuXcgioQoWWsB'}
+          partnerOrderId={'fd1b4a14-f4be-4218-9e76-4aec9c00db69'}
+        />,
+      ),
     );
     expect(wrapper.dive()).toMatchSnapshot();
   });
