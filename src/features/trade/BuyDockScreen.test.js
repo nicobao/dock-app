8dc5046912268cdf8ce72b59b0bf4a6669b22c1e
Ifeanyi Osinakayah <ifeanyiosinakayah15@gmail.com>
2022-04-09 03:56:50 +0100

    Snapshots for different transak states

--- src/features/trade/BuyDockScreen.test.js
+++ src/features/trade/BuyDockScreen.test.js
@@ -4,7 +4,11 @@ import configureMockStore from 'redux-mock-store';
 import {BuyDockScreenScreen} from './BuyDockScreen';
 import thunk from 'redux-thunk';
 import {renderAppProviders} from '../../core/test-utils';
-import TransakProvider from './components/TransakProvider';
+import TransakProvider, {
+  TransakIntroView,
+  TransakWebView,
+} from './components/TransakProvider';
+import queryString from 'query-string';
 
 const mockStore = configureMockStore([thunk]);
 
@@ -35,6 +39,23 @@ describe('BuyDockScreenScreen', () => {
         />,
       ),
     );
+    expect(wrapper.dive()).toMatchSnapshot();
+  });
+  it('should render TransakIntro Component', () => {
+    const wrapper = shallow(
+      renderAppProviders(<TransakIntroView onPress={jest.fn()} />),
+    );
+
+    expect(wrapper.dive()).toMatchSnapshot();
+  });
+
+  it('should render TransakProvider Webview', () => {
+    const queryUrl = queryString.stringify({
+      partnerOrderId: 'fd1b4a14-f4be-4218-9e76-4aec9c00db69',
+      cryptoCurrencyCode: 'DOCK',
+      walletAddress: '3C7Hq5jQGxeYzL7LnVASn48tEfr6D7yKtNYSuXcgioQoWWsB',
+    });
+    const wrapper = shallow(<TransakWebView queryUrl={queryUrl} />);
     expect(wrapper.dive()).toMatchSnapshot();
   });
 });
