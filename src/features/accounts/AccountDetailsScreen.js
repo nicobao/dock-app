869a8946c623c9b51f2a1cfa8d769477df2d19b2
Ifeanyi Osinakayah <ifeanyiosinakayah15@gmail.com>
2022-04-14 20:56:15 +0100

    Test if account object is properly set

--- src/features/accounts/AccountDetailsScreen.js
+++ src/features/accounts/AccountDetailsScreen.js
@@ -473,7 +473,7 @@ export function AccountDetailsContainer({route}) {
     dispatch(transactionsOperations.loadTransactions(accountId));
   }, [dispatch, accountId]);
 
-  if (!account) {
+  if (!('@context' in account)) {
     return <LoadingScreen />;
   }
 
