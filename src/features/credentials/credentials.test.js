0abb807ca943bbb151464605392905fa64fb66c9
Maycon Mellos <maycon.mellos@gmail.com>
2022-04-19 12:30:55 -0300

    testing useCredentials hook

--- src/features/credentials/credentials.test.js
+++ src/features/credentials/credentials.test.js
@@ -1,5 +1,29 @@
-import {sortByIssuanceDate, getCredentialTimestamp} from './credentials';
+import {renderHook} from '@testing-library/react-hooks';
+import {
+  sortByIssuanceDate,
+  getCredentialTimestamp,
+  useCredentials,
+} from './credentials';
+import {Credentials} from '@docknetwork/wallet-sdk-credentials/lib';
 
+const mockCreds = [
+  {
+    id: 1,
+    issuanceDate: '2022-03-25T10:28:18.848Z',
+  },
+  {
+    id: 0,
+    issuanceDate: '2023-03-25T10:28:18.848Z',
+  },
+  {
+    id: 2,
+    issuanceDate: '2020-03-25T10:28:18.848Z',
+  },
+  {
+    id: 3,
+    issuanceDate: null,
+  },
+];
 describe('Credentials helpers', () => {
   describe('getCredentialTimestamp', () => {
     it('expect to get credential timestamp', () => {
@@ -31,28 +55,29 @@ describe('Credentials helpers', () => {
 
   describe('sortByIssuanceDate', () => {
     it('expect to sort credentials', () => {
-      const creds = [
-        {
-          id: 1,
-          issuanceDate: '2022-03-25T10:28:18.848Z',
-        },
-        {
-          id: 0,
-          issuanceDate: '2023-03-25T10:28:18.848Z',
-        },
-        {
-          id: 2,
-          issuanceDate: '2020-03-25T10:28:18.848Z',
-        },
-        {
-          id: 3,
-          issuanceDate: null,
-        },
-      ];
-
-      const result = creds.sort(sortByIssuanceDate);
+      const result = mockCreds.sort(sortByIssuanceDate);
 
       result.forEach((item, idx) => expect(item.id).toEqual(idx));
+    });
+  });
+
+  describe('useCredentials', () => {
+    const creds = [...mockCreds];
+
+    beforeAll(async () => {
+      jest.spyOn(creds, 'sort');
+      jest
+        .spyOn(Credentials.getInstance(), 'query')
+        .mockImplementation(() => Promise.resolve(creds));
+      await renderHook(() => useCredentials());
+    });
+
+    it('expect to fetch credentials', () => {
+      expect(Credentials.getInstance().query).toBeCalled();
+    });
+
+    it('expect to sort credentials', () => {
+      expect(creds.sort).toBeCalled();
     });
   });
 });
