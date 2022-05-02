3df932cf22b04920f19a8bbc5c29064ff9ea698e
Ifeanyi Osinakayah <ifeanyiosinakayah15@gmail.com>
2022-05-02 13:41:30 +0100

    Custom analytics event

--- src/features/dev-settings/DevSettingsScreen.js
+++ src/features/dev-settings/DevSettingsScreen.js
@@ -27,6 +27,7 @@ import {
 } from '../app/app-slice';
 import {UtilCryptoRpc} from '@docknetwork/react-native-sdk/src/client/util-crypto-rpc';
 import {FeatureFlags, getAllFeatures, useFeatures} from '../app/feature-flags';
+import {ANALYTICS_EVENT, logAnalyticsEvent} from '../analytics/analytics-slice';
 
 type Props = {
   onAddAccount: any,
@@ -158,6 +159,9 @@ export function DevSettingsScreen({
                 setShowNetworkOptions(false);
                 onNetworkChange(networkId)
                   .then(() => {
+                    logAnalyticsEvent(ANALYTICS_EVENT.SETTINGS.SWITCH_NETWORK, {
+                      networkId,
+                    });
                     showToast({
                       message: translate('dev_settings.switch_network_success'),
                       type: 'success',
@@ -167,6 +171,10 @@ export function DevSettingsScreen({
                     showToast({
                       message: translate('dev_settings.switch_network_error'),
                       type: 'error',
+                    });
+                    logAnalyticsEvent(ANALYTICS_EVENT.FAILURES, {
+                      networkId,
+                      name: ANALYTICS_EVENT.SETTINGS.SWITCH_NETWORK,
                     });
                   });
               }}>
