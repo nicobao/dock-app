1f2720976d62b1d4d0bc1ae93a7ec8bda6014fb1
Maycon Mello <maycon.mellos@gmail.com>
2022-08-16 12:45:40 -0300

    fix issue with export did

--- src/features/didManagement/didHooks.test.js
+++ src/features/didManagement/didHooks.test.js
@@ -95,7 +95,7 @@ describe('DID hooks', () => {
     });
     const {result: dIDManagementResult} = renderHook(() => useDIDManagement());
     expect(dIDManagementResult.current.deleteDID).toBeCalledWith({
-      id: 'did-key-z6MkjjCpsoQrwnEmqHzLdxWowXk5gjbwor4urC1RPDmGeV8r',
+      id: 'did:key:z6MkjjCpsoQrwnEmqHzLdxWowXk5gjbwor4urC1RPDmGeV8r',
     });
   });
   //
@@ -233,7 +233,7 @@ describe('DID hooks', () => {
     });
     expect(Share.open).toBeCalled();
     expect(RNFS.writeFile).toBeCalledWith(
-      'DocumentDirectoryPath/did_did:key:z6MkjjCpsoQrwnEmqHzLdxWowXk5gjbwor4urC1RPDmGeV8r.json',
+      'DocumentDirectoryPath/did_did-key-z6MkjjCpsoQrwnEmqHzLdxWowXk5gjbwor4urC1RPDmGeV8r.json',
       '{}',
     );
   });
