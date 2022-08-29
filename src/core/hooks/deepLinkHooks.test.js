fbba0e88360f50ffa52e2490090fc0e3bbfe4eee
Ifeanyi Osinakayah <ifeanyiosinakayah15@gmail.com>
2022-08-29 14:24:23 +0100

    tests

--- src/core/hooks/deepLinkHooks.test.js
+++ src/core/hooks/deepLinkHooks.test.js
@@ -4,6 +4,9 @@ import {Routes} from '../routes';
 import {navigate} from '../navigation';
 
 describe('Deep hooks', () => {
+  beforeEach(() => {
+    jest.resetAllMocks();
+  });
   it('should navigate to presentation screen', () => {
     const {result} = renderHook(() => useDeepLink({isLoggedIn: true}));
     act(() => {
@@ -15,5 +18,20 @@ describe('Deep hooks', () => {
     expect(navigate).toBeCalledWith(Routes.CREDENTIALS_SHARE_AS_PRESENTATION, {
       deepLinkUrl: 'dockwallet://proof-request?url=https&nonce=1',
     });
+  });
+  it('should navigate to presentation screen when not logged in', () => {
+    const {result} = renderHook(() => useDeepLink({isLoggedIn: false}));
+    act(() => {
+      result.current.gotoScreenDeepLink({
+        url: 'dockwallet://proof-request?url=https&nonce=1',
+      });
+    });
+
+    expect(navigate).not.toBeCalledWith(
+      Routes.CREDENTIALS_SHARE_AS_PRESENTATION,
+      {
+        deepLinkUrl: 'dockwallet://proof-request?url=https&nonce=1',
+      },
+    );
   });
 });
