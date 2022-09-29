a9b0d7d6fb59c9cd0758c1c8bbfba76536a56a4f
Maycon Mello <maycon.mellos@gmail.com>
2022-09-29 11:05:06 -0300

    fixing iOS build issues

--- scripts/fix-deps.sh
+++ scripts/fix-deps.sh
@@ -2,3 +2,7 @@ rm -rf node_modules/@docknetwork/wallet-sdk-core/node_modules/realm
 rm -rf node_modules/@docknetwork/wallet-sdk-react-native/node_modules/realm
 rm -rf node_modules/@docknetwork/wallet-sdk-core/node_modules/realm
 rm -rf node_modules/@docknetwork/wallet-sdk-react-native/node_modules/realm
+rm -rf node_modules/@docknetwork/wallet-sdk-react-native/node_modules/@react-native-community
+rm -rf node_modules/@docknetwork/wallet-sdk-react-native/node_modules/@react-native
+rm -rf node_modules/@docknetwork/wallet-sdk-react-native/node_modules/react-native
+rm -rf node_modules/@docknetwork/wallet-sdk-react-native/node_modules/jsc-android
