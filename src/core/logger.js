03670c21d98ab972a7a3d0cab084a1b38e127f41
Maycon Mello <maycon.mellos@gmail.com>
2022-10-04 16:16:15 -0300

    fixing unit tests

--- src/core/logger.js
+++ src/core/logger.js
@@ -1,7 +1,8 @@
+import {LOGGER_LEVEL} from '@env';
 const winston = require('winston');
 
 export const Logger = winston.createLogger({
   format: winston.format.simple(),
-  level: 'info',
+  level: LOGGER_LEVEL || 'info',
   transports: [new winston.transports.Console()],
 });
