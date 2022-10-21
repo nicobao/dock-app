b7ef7cb4aa23a70d983b90999a7a16f93160dfd3
Ifeanyi Osinakayah <ifeanyiosinakayah15@gmail.com>
2022-10-21 16:33:39 +0100

    Provide test

--- src/features/app/BuildIdentifier.test.js
+++ src/features/app/BuildIdentifier.test.js
@@ -2,18 +2,27 @@ import React from 'react';
 import {shallow} from 'enzyme';
 import configureMockStore from 'redux-mock-store';
 import {BuildIdentifier} from './BuildIdentifier';
+import {Provider} from 'react-redux';
 
 const mockStore = configureMockStore();
 
+const ReduxProvider = ({children, reduxStore}) => (
+  <Provider store={reduxStore}>{children}</Provider>
+);
 describe('BuildIdentifier', () => {
   it('should render correctly', () => {
     const initialState = {
       // placeholder for redux store
     };
 
-    const wrapper = shallow(<BuildIdentifier onUnlock={jest.fn()} />, {
-      context: {store: mockStore(initialState)},
-    });
+    const wrapper = shallow(
+      <ReduxProvider reduxStore={mockStore(initialState)}>
+        <BuildIdentifier onUnlock={jest.fn()} />
+      </ReduxProvider>,
+      {
+        context: {store: mockStore(initialState)},
+      },
+    );
     expect(wrapper.dive()).toMatchSnapshot();
   });
 });
