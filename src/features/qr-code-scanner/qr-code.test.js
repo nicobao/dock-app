4230d74c0e2de986584aeaf6270ae27415df3473
Ifeanyi Osinakayah <ifeanyiosinakayah15@gmail.com>
2022-11-02 23:46:16 +0100

    Confirmation modal before scanning invalid credentials

--- src/features/qr-code-scanner/qr-code.test.js
+++ src/features/qr-code-scanner/qr-code.test.js
@@ -145,8 +145,18 @@ describe('qr-code', () => {
 
     it('expect to add credential from url', async () => {
       const credentialData = {
-        id: 'test',
-        content: 'some-data',
+        '@context': ['https://www.w3.org/2018/credentials/v1'],
+        id: 'https://creds.dock.io/8e02c35ae370b02f47d7faaf41cb1386768fc75c9fca7caa6bb389dbe61260eb',
+        type: ['VerifiableCredential', 'UniversityDegreeCredential'],
+        credentialSubject: {},
+        issuanceDate: '2022-06-27T12:08:30.675Z',
+        expirationDate: '2029-06-26T23:00:00.000Z',
+        issuer: {
+          name: 'John Doe',
+          description: '',
+          logo: '',
+          id: 'did:dock:5CJaTP2eGCLf5ZNPUXYbWxUvJQMTseKfc4hi8WVBC1K8eW9N',
+        },
       };
 
       jest
